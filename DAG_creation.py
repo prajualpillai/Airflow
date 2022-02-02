b6e89afc22698c4155ca014af07ea12b87710528
prajual <prajualpillai@gmail.com>
2022-02-02 16:34:00 +0530

    Basic DAG added

---
+++ DAG_creation.py
@@ -0,0 +1,37 @@
+from airflow.models import DAG
+from airflow.operators import python_operator
+from datetime import datetime, timedelta
+
+# Defining deafult arguments for DAG
+default_dag_args = {
+    'owner': 'prajual',
+    'start_date': datetime(2022, 1, 28),
+    'retry_delay': timedelta(minutes=5),
+    'retries': 1
+}
+
+with DAG(
+        'sample_test',
+        schedule_interval="@once",
+        default_args=default_dag_args) as dag:
+    # Defining functions which will be called while running the DAG
+    def greeting():
+        import logging
+        logging.info("Hi python")
+
+
+    def greeting2():
+        import logging
+        logging.info("bye python")
+
+
+    # Defining tasks through which the functions are called
+    op1 = python_operator.PythonOperator(task_id='greet1',
+                                         dag=dag,
+                                         python_callable=greeting)
+    op2 = python_operator.PythonOperator(task_id='greet2',
+                                         dag=dag,
+                                         python_callable=greeting2)
+
+    # Defining the order in which the tasks will be run
+    op1 >> op2
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Airflow
+
+## DAG:
+
+    In Airflow, a DAG – or a Directed Acyclic Graph – is a collection of all the tasks you want to run, organized in a way that reflects their relationships and dependencies.
